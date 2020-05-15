808be210c18c3ae5a2e9559f754c4268754ca6f2
Jason Plum <jplum@gitlab.com>
2020-05-15 09:37:38 -0400

    RSpec: add HelmTemplate helper and initial Redis
    I ran into some oddities as I was working
    https://gitlab.com/gitlab-org/charts/gitlab/-/merge_requests/1335
    
    It made no sense to me to keep trying to manually test and verify
    upwards of six cases while I was testing various methods, let alone the
    maddening frustration of compatibility in versions :cry:
    
    So, thanks to prior work by @smcgivern, I've implemented an ingester
    of the YAML output of `helm template`, and now we have a means to
    directly test the output of out chart!
    
    ```Ruby
    output = HelmTemplate.new(values)
    resque = output.dig('ConfigMap/test-webservice','data','resque.yml.erb')
    expect(resque).not_to include("redis/redis-password")
    ```

---
+++ spec/helm_template_helper.rb
@@ -0,0 +1,55 @@
+require 'yaml'
+require 'open3'
+
+class HelmTemplate
+  def initialize(values)
+    template(values)
+  end
+
+  def template(values)
+    @values  = values
+    result = Open3.capture3('helm template test . -f -',
+                             chdir: File.join(__dir__,  '..'),
+                             stdin_data: YAML.dump(values))
+    @stdout, @stderr, @exit_code = result
+    # load the complete output's YAML documents into an array
+    yaml = YAML.load_stream(@stdout)
+    # filter out any empty YAML documents (nil)
+    yaml.select!{ |x| !x.nil? }
+    # create an indexed Hash keyed on Kind/metdata.name
+    @mapped = yaml.to_h  { |doc|
+      [ "#{doc['kind']}/#{doc['metadata']['name']}" , doc ]
+    }
+  end
+
+  def dig(*args)
+    @mapped.dig(*args)
+  end
+
+  def volumes(item)
+    @mapped.dig(item,'spec','template','spec','volumes')
+  end
+
+  def find_volume(item, volume_name)
+    volumes = volumes(item)
+    volumes.keep_if { |volume| volume['name'] == volume_name }
+    volumes[0]
+  end
+
+  def projected_volume_sources(item,volume_name)
+    volume = find_volume(item,volume_name)
+    volume['projected']['sources']
+  end
+
+  def exit_code()
+    @exit_code.to_i
+  end
+
+  def stderr()
+    @stderr
+  end
+
+  def values()
+    @values
+  end
+end
